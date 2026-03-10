80769ff281d6e313a2d09992da92a01779fe8138
mateussperess <mateuspereslopesl@outlook.com>
2026-03-09 21:52:48 -0300

    adding new intent

---
+++ app/src/main/java/com/example/atividade_dm2_lista_contatos/CriarContato.java
@@ -0,0 +1,24 @@
+package com.example.atividade_dm2_lista_contatos;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CriarContato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_criar_contato);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainCriarContato), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/atividade_dm2_lista_contatos/MainActivity.java
+++ app/src/main/java/com/example/atividade_dm2_lista_contatos/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.atividade_dm2_lista_contatos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,12 +17,23 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Contato> contatos;
+    Button btnCriarContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnCriarContato = findViewById(R.id.btnCriarContato);
+        btnCriarContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itCriarContato = new Intent(MainActivity.this, CriarContato.class);
+                startActivity(itCriarContato);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
